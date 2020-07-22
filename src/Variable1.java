29649621350bbc38fd94446ab17d898cf3d5a732
palaksinghania05 <palak.singhania_cs.ccv19@gla.ac.in>
2020-07-22 21:42:53 +0530

    Initial commit

---
+++ src/Variable1.java
@@ -0,0 +1,13 @@
+public class Variable1 {
+    public static void main(String[] args) {
+        //variable declaration
+        int rollNumber;
+        //variable initialization
+        rollNumber = 51;
+        //variable declaration and initialization both
+        int rank = 1;
+        //Output1
+        System.out.println(rollNumber);
+        System.out.println(rank);
+    }
+}
---
+++ src/Variable2.java
@@ -0,0 +1,13 @@
+public class Variable2 {
+    public static void main(String[] args) {
+        //variable declaration
+        int rollNumber;
+        //variable initialization
+        rollNumber = 51;
+        //variable declaration and initialization both
+        int rank = 1;
+        //Output2
+        System.out.println("Roll Number : " + rollNumber);
+        System.out.println("Rank : " + rank);
+    }
+}
